fac889b36e08bd39bacdf3b50e3d86fbd48efee0
atoyegbe <herdeyemie@gmail.com>
2020-05-26 11:22:13 -0700

    Intial commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
