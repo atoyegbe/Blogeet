0367e26f8f7fc6297649fa38a49c7d4241820446
atoyegbe <herdeyemie@gmail.com>
2020-05-27 05:58:19 -0700

    changes

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
 
 
